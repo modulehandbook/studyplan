6a20f32499034199497286d66363376880c6d6b0
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2021-12-02 15:44:15 +0100

    fix mongoose deprication warning

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 const mongodbURI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/studyplan";
 
-mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+mongoose.connect(mongodbURI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then( //useCreateIndex: true because: ("https://github.com/Automattic/mongoose/issues/6890")
   () => {
     console.log("Database is connected");
   },
