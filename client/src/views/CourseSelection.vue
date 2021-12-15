7f92d46ec6d3b65fa6d689585fbf90d1748caa80
LennoxCode <>
2021-12-15 15:23:15 +0100

    added import for header in the script

--- client/src/views/CourseSelection.vue
+++ client/src/views/CourseSelection.vue
@@ -1,3 +1,10 @@
 <template>
   <BaseHeading><h1>Hier ist Die Seite zum Belegen</h1></BaseHeading>
   </template>
+
+  <script>
+  import BaseHeading from "../components/BaseHeading.vue";
+  export default {
+    components: { BaseHeading },
+  };
+  </script>
--- client/src/views/CourseSurvey.vue
+++ client/src/views/CourseSurvey.vue
@@ -1,3 +1,11 @@
 <template>
-  <BaseHeading><h1>Hier ist die Seite zur Umfrage</h1></BaseHeading>
+  <BaseHeading><h1>Hier ist die Seite zur Umfrage </h1></BaseHeading>
+
   </template>
+  
+  <script>
+  import BaseHeading from "../components/BaseHeading.vue";
+  export default {
+    components: { BaseHeading },
+  };
+  </script>
