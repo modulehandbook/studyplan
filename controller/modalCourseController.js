38b13793cddc63c8eb3efb1bc9303d3d6e3c8876
LennoxCode <leonard.valentin1999@gmail.com>
2022-01-03 21:11:33 +0100

    added the model and controller

---
+++ controller/modalCourseController.js
@@ -0,0 +1,59 @@
+const User = require("../model/user"),
+ModalCourse = require("../model/modalCourse");
+module.exports = {
+  create: (req, res) => {
+    let modalCourseParams = {
+      name: req.body.name,
+      code: req.body.code,
+      semester: req.body.semester,
+      availablePlaces: req.body.availablePlaces,
+    };
+    ModalCourse.create(modalCourseParams)
+      .then((modalCourse) => {
+        res.json(modalCourse);
+      })
+      .catch((error) => {
+        console.log(`Error saving ModalCourse: ${error.message}`);
+        return;
+      });
+  },
+  delete: (req, res) => {
+    ModalCourse.findByIdAndRemove(req.params.id)
+      .then(async () => {
+      })
+      .catch((error) => {
+        console.log(`Error deleting ModalCourse by ID: ${error.message}`);
+        return;
+      });
+  },
+  show: (req, res) => {
+    ModalCourse.findById(req.params.id)
+        .then((modalCourse) => {
+            res.json(modalCourse);
+        })
+      .catch((error) => {
+        console.log(`Error fetching modalCourse by ID: ${error.message}`);
+      });
+  },
+
+  update: (req, res) => {
+    let modalCourseId = req.params.id;
+
+    ModalCourse.findByIdAndUpdate(
+        modalCourseId,
+      {
+        $set: {
+          students: req.body.students,
+        },
+      },
+      { new: true }
+    )
+      .populate("semester")
+      .then((courseSelection, err) => {
+        if (err) console.log(err.message);
+        else {
+          res.json(courseSelection);
+        }
+      });
+  },
+};
