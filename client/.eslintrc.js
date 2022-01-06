2aec740c3316bfa2a9774bc6c8f2e2c40370fcde
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-01-06 16:15:20 +0100

    edit eslint config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-    root: true,
-    env: {
-      node: true
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "prettier",
+    "eslint:recommended",
+    "@vue/prettier",
+  ],
+  plugins: ["prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": ["warn"],
+    "vue/multi-word-component-names": "off",
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
     },
-    extends: [
-      "plugin:vue/essential",
-      "plugin:prettier/recommended",
-      "eslint:recommended",
-      "@vue/prettier"
-    ],
-    parserOptions: {
-      ecmaVersion: 2020
-    },
-    rules: {
-      "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-      "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-    },
-    overrides: [
-      {
-        files: [
-          "**/__tests__/*.{j,t}s?(x)",
-          "**/tests/unit/**/*.spec.{j,t}s?(x)"
-        ],
-        env: {
-          jest: true
-        }
-      }
-    ]
-  };
-  
+  ],
+};
