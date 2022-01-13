008c1881c7da57f94a234724afb2bc317f124105
ChuChuPL <linhchu11489@gmail.com>
2022-01-13 01:33:25 +0100

    Impressum/Datenschutz

--- client/src/views/Imprint.vue
+++ client/src/views/Imprint.vue
@@ -2,20 +2,20 @@
   <div>
     <BaseHeading><h1>Impressum</h1> </BaseHeading>
     <div class="container">
-      <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
-      <p>
-        Julia Zamaitat<br />
-        Marienstra&szlig;e 2<br />
-        12459 Berlin
-      </p>
+
       <h2>Kontakt</h2>
-      <p>
-        Telefon: 017620786909<br />
-        E-Mail: j.zamaitat [@] gmail.com
-      </p>
-      <p>
-        Quelle: <a href="https://www.e-recht24.de">https://www.e-recht24.de</a>
-      </p>
+      <a
+            href="https://home.htw-berlin.de/~kleinen/contact/"
+            target="blank"
+            >Prof. Dr. Barne Kleinen</a
+      >
+
+      <h2>Informationen zum Projekt-Programm</h2>
+      <a
+            href="https://showtime.f4.htw-berlin.de/"
+            target="blank"
+            >Showtime</a
+      >
     </div>
   </div>
 </template>
@@ -25,4 +25,5 @@
   width: 80%;
   margin: 0 auto;
 }
+a {color:blue;}
 </style>
