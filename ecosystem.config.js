4eb367a834a6be264a719471bc99cd3ea56c4b2e
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-01-17 17:45:00 +0100

    add pm2 config for prod

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name   : "node-server",
+    script : "./main.js",
+    env: {
+	"NODE_ENV": "production",
+	"MONGODB_URI: "mongodb://localhost:27017/studyplan",
+	"PORT": "3000",
+	"TEST_PROD: "false",
+    }
+  }]
+}
