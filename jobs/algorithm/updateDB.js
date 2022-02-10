b132f9443550a4f3d699f28cba3feeca67f5eaaf
BenSauerlaender <Ben.Sauerlaender@Student.HTW-Berlin.de>
2022-02-10 19:17:08 +0100

    showtime quickfix

--- jobs/algorithm/updateDB.js
+++ jobs/algorithm/updateDB.js
@@ -1,9 +1,11 @@
 const ModalCourse = require("../../model/modalCourse");
 const User = require("../../model/user");
 const Semester = require("../../model/semester");
+require("../../model/courseSelection");
 module.exports.updateDB = async (solution, currentSemester) => {
   const semester = await Semester.find({ name: currentSemester });
   const updateCourse = async function (code, students) {
+	  console.log(students)
     for (const i in students) {
       const email = students[i];
       students[i] = await User.findOne({ email: email }, "_id").clone(); //TODO: dont know why i need clone(); maybe thats not good practice
@@ -20,7 +22,7 @@ module.exports.updateDB = async (solution, currentSemester) => {
     ).catch((err) => console.log(err));
   };
   for (const [code, students] of Object.entries(solution)){
-    await updateCourse(code, students);
+    await updateCourse(code, students.map((s) => s.email));
   }
 
 };
